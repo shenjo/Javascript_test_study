75b504de3eb6d37dbec67a77f5296e0dcf203d96
shenhuan <shenhuan.sh@quadtalent.com>
2023-03-28 11:42:55 +0800

    数据源

--- client/modules/chart-designer/components/data-source/JsonDataSource.tsx
+++ client/modules/chart-designer/components/data-source/JsonDataSource.tsx
@@ -1,20 +1,19 @@
-import { Input } from 'antd';
+import { Input, Select } from 'antd';
+import { amountRatePerSaleGroupIn2022, salesInfoPerYear, salesPerYearPerGroup } from '@/mock/mockSaleData';
 
+const values = [salesInfoPerYear, salesPerYearPerGroup, amountRatePerSaleGroupIn2022];
 export default function JsonDataSource (props: ChartSetting) {
   const { value, onChange } = props;
 
-  const onDataChange = (event?: any) => {
-    const value = event.target.value;
-    if (value) {
-      onChange([{
-        source: JSON.parse(value)
-      }]);
-    }
+  const onValueChange = (value: number) => {
+    onChange(values[value]);
   };
 
   return (
-    <Input onChange={onDataChange}>
-
-    </Input>
+    <Select onChange={onValueChange} options={[
+      { label: '公司年度销售数据', value: 0 },
+      { label: '每个小组每年的销售情况', value: 1 },
+      { label: '22年度所有销售小组的销售额占比', value: 2 }
+    ]} />
   );
 }
